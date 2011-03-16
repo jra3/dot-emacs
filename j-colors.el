d3c89702022ef486e1a7c8dcb8749d6aa295c439
jallen <jallen@facebook.com>
2011-03-15 23:46:47 -0700

    fixing colors

--- j-colors.el
+++ j-colors.el
@@ -5,7 +5,7 @@
 
 ;; select theme - first list element is for windowing system, second is for console/terminal
 ;; Source : http://www.emacswiki.org/emacs/ColorTheme#toc9
-(setq color-theme-choices '(color-theme-dark-laptop color-theme-dark-laptop))
+(setq color-theme-choices '(color-theme-tangotango color-theme-dark-laptop))
 
 ;; test for each frame or console
 (require 'cl)
@@ -13,14 +13,9 @@
   (defun test-win-sys (frame)
     (let ( (color-theme-is-global nil) )
       (select-frame frame)
-      (eval (append '(if (window-system frame)) 
+      (eval (append '(if (window-system frame))
                     (mapcar (lambda (x) (cons x nil)) cols))))))
 ;; hook on after-make-frame-functions
 (add-hook 'after-make-frame-functions 'test-win-sys)
-
-
-
-;; needs to occur after color theme is set since color-themes are inexplicably
-;; stripping fonts defined in .Xresources
 
 (provide 'j-colors)
