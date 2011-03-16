1c45a7e7121a5520c22b345a6b18e8418bbd898c
jallen <jallen@facebook.com>
2011-03-16 00:21:05 -0700

    fixing color junk...

--- j-colors.el
+++ j-colors.el
@@ -1,21 +1,17 @@
 (require 'color-theme)
 (color-theme-initialize)
-
 (require 'color-theme-tangotango)
 
-;; select theme - first list element is for windowing system, second is for console/terminal
-;; Source : http://www.emacswiki.org/emacs/ColorTheme#toc9
-(setq color-theme-choices '(color-theme-tangotango color-theme-dark-laptop))
+(color-theme-dark-laptop)
+(defun apply-color-theme (frame)
+  "Apply color theme to a frame based on whether its a 'real'
+    window or a console window."
+  (let ((color-theme-is-global nil))    
+    (select-frame frame)
+    (if (window-system frame)
+	(color-theme-tangotango)
+      (color-theme-dark-laptop))))
 
-;; test for each frame or console
-(require 'cl)
-(lexical-let ( (cols color-theme-choices) )
-  (defun test-win-sys (frame)
-    (let ( (color-theme-is-global nil) )
-      (select-frame frame)
-      (eval (append '(if (window-system frame))
-                    (mapcar (lambda (x) (cons x nil)) cols))))))
-;; hook on after-make-frame-functions
-(add-hook 'after-make-frame-functions 'test-win-sys)
+(add-hook 'after-make-frame-functions 'apply-color-theme)
 
 (provide 'j-colors)
