c6c294c1307387e2abef3346498ce4999c0fb7e9
jallen <jallen@fb.com>
2012-04-03 16:22:18 -0700

    org off Dropbox

--- j-org.el
+++ j-org.el
@@ -1,10 +1,10 @@
 (require 'org-install)
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(setq org-directory "~/Dropbox/org")
+(setq org-directory "~/org")
 (setq org-mobile-directory "~/Dropbox/MobileOrg/")
 
-(setq org-agenda-files (quote ("~/Dropbox/org/work.org")))
-(setq org-mobile-inbox-for-pull "~/Dropbox/org/index.org")
+(setq org-agenda-files (quote ("~/org/work.org")))
+(setq org-mobile-inbox-for-pull "~/org/index.org")
 
 (provide 'j-org)
