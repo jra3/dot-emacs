fb0abc9910232b505efe3a49e82602fc7bbc6a3d
jallen <jallen@fb.com>
2012-07-21 16:31:56 -0700

    Split terminal compatability into its own module

--- j-globalkeys.el
+++ j-globalkeys.el
@@ -58,20 +58,4 @@
   (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
 (global-set-key (kbd "C-c +") 'increment-number-at-point)
 
-(defadvice terminal-init-xterm (after map-S-up-escape-sequence activate)
-
-  (define-key input-decode-map "\e[1;9A" [M-up])
-  (define-key input-decode-map "\e[1;9B" [M-down])
-  (define-key input-decode-map "\e[1;9C" [M-right])
-  (define-key input-decode-map "\e[1;9D" [M-left])
-
-  (define-key input-decode-map "\e[1;10A" [M-S-up])
-  (define-key input-decode-map "\e[1;10B" [M-S-down])
-  (define-key input-decode-map "\e[1;10C" [M-S-right])
-  (define-key input-decode-map "\e[1;10D" [M-S-left])
-
-  ;; weird quirk
-  (global-set-key [select] [S-up])
-)
-
 (provide 'j-globalkeys)
