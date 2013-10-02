039138ae72c10b2873be66a344ed5e178ca84a41
John Allen <john@interana.com>
2013-10-02 04:38:56 +0000

    org S-arrows

--- j-org.el
+++ j-org.el
@@ -2,12 +2,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (setq org-directory "~/org")
-(setq org-mobile-directory "~/Dropbox/MobileOrg/")
+(setq org-replace-disputed-keys t)
 
-(setq org-agenda-files (quote ("~/org/work.org" "~/org/personal.org" "~/org/irc.org")))
-(setq org-mobile-inbox-for-pull "~/org/captured.org")
-
-(setq org-mobile-use-encryption t)
-(setq org-mobile-encryption-password (getenv "MOBILE_ORG_PW"))
+;; (setq org-agenda-files (quote ("~/org/work.org" "~/org/personal.org" "~/org/irc.org")))
 
 (provide 'j-org)
