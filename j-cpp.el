4d8e57c05815c087a4657dbbe831ebf627ee8edc
John Allen <john@interana.com>
2013-10-02 23:26:37 +0000

    google style is the best style

--- j-cpp.el
+++ j-cpp.el
@@ -3,15 +3,20 @@
 
 (require 'j-completion)
 (require 'highlight-80+)
+(require 'google-c-style)
 
 (add-hook 'c++-mode-hook
           (lambda ()
-            (setq compilation-auto-jump-to-first-error t)
-            (define-key c++-mode-map (kbd "C-c o") 'ff-get-other-file)
             (highlight-80+-mode t)
+            ;; (c-set-offset 'arglist-intro '+)
+            ;; (c-set-offset 'innamespace '-)
+            (define-key c++-mode-map (kbd "C-c o") 'ff-get-other-file)
             (subword-mode 1)
-            (c-set-offset 'arglist-intro '+)
-            (c-set-offset 'innamespace '-)
-            (setq require-final-newline t)))
+            (setq require-final-newline t)
+            (setq compilation-auto-jump-to-first-error t)
+            ))
+
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
 
 (provide 'j-cpp)
