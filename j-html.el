5368a68efff9f77017bf9fbe6fe185e6c1d97e71
John Allen <jallen@porcnick.com>
2013-12-16 15:10:01 -0500

    asdf

--- j-html.el
+++ j-html.el
@@ -1,6 +1,4 @@
 (require 'rainbow-mode)
-(require 'zencoding-mode)
-(add-hook 'html-mode-hook (lambda () (zencoding-mode t)))
 (add-hook 'html-mode-hook (lambda () (rainbow-mode t)))
 
 (provide 'j-html)
