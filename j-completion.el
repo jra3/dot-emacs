ca1dcd7c0309e7f4d71febdb7b49c3ec4c0279c5
John Allen <jallen@porcnick.com>
2014-05-13 15:15:49 -0400

    have python completion working somehow

--- j-completion.el
+++ j-completion.el
@@ -1,19 +1,8 @@
-;; (defun indent-or-expand (arg)
-;;   "Either indent according to mode, or expand the word preceding point."
-;;   (interactive "*P")
-;;   (if (and
-;;        (or (bobp) (= ?w (char-syntax (char-before))))
-;;        (or (eobp) (not (= ?w (char-syntax (char-after))))))
-;;       (dabbrev-expand arg)
-;;     (indent-according-to-mode)))
+;; Jedi for python
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+(setq jedi:server-command '("~/.emacs.d/el-get/jedi/jediepcserver.py"))
 
-;; (defun my-tab-fix ()
-;;   (local-set-key [tab] 'indent-or-expand))
-
-;; (add-hook 'php-mode-hook 'my-tab-fix)
-;; (add-hook 'c-mode-hook          'my-tab-fix)
-;; (add-hook 'sh-mode-hook         'my-tab-fix)
-;; (add-hook 'emacs-lisp-mode-hook 'my-tab-fix)
-;; more mode hooks, yada yada, etc ...
+(require 'company) ;; cool for clang
 
 (provide 'j-completion)
