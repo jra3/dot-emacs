439946ab4fdda311035f92315407e35a6b957822
John Allen <john@interana.com>
2016-04-09 17:55:40 -0400

    notifier

--- j-custom.el
+++ j-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(alert-user-configuration (quote ((nil notifier nil))))
  '(c-basic-offset 2)
  '(case-fold-search t)
  '(column-number-mode 1)
