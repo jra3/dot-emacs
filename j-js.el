d4a3c48c6aeb17375224a5e26c678b85a3beb43b
John Allen <jallen@fb.com>
2017-04-10 22:13:09 -0700

    tweak

--- j-js.el
+++ j-js.el
@@ -44,6 +44,12 @@
   (append flycheck-disabled-checkers
     '(json-jsonlist)))
 
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+    ad-do-it))
+
 ;; =============================================================================
 
 (provide 'j-js)
