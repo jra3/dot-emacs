b3711e9de31c22348b3d3d7ebd405141ff782951
John Allen <jallen@fb.com>
2017-05-26 11:00:00 -0700

    hack is eating my life

--- j-php.el
+++ j-php.el
@@ -1,41 +1,17 @@
-;; (require 'php-mode)
-;; (require 'xhp-mode)
-;; (require 'highlight-80+)
-
-(require 'js2-mode)
-(require 'xhp-mode)
 (require 'hack-mode)
+(require 'xhp-mode)
 (require 'highlight-80+)
 
-(defun ywb-php-lineup-arglist-intro (langelem)
-  (save-excursion
-    (goto-char (cdr langelem))
-    (vector (+ (current-column) c-basic-offset))))
-
-(defun ywb-php-lineup-arglist-close (langelem)
-  (save-excursion
-    (goto-char (cdr langelem))
-    (vector (current-column))))
-
-(defconst fb-php-style
-  '((c-basic-offset . 2)
-    (c-offsets-alist . ((substatement-open . 0)
-                        )))
-  "Facebook's PHP Programming style")
-(c-add-style "fb-php-style" fb-php-style)
-
 (let
-    ((mode-hook (lambda ()
-                  (c-set-style "fb-php-style")
-                  (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
-                  (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)
-                  
-                  (highlight-80+-mode t)
+x    ((mode-hook (lambda ()
+                  (c-set-style "hack-style")
                   (set (make-local-variable 'require-final-newline) t)
                   )))
 
   (add-hook 'xhp-mode-hook mode-hook)
   (add-hook 'hack-mode-hook mode-hook)
-)
 
+  )
+
+(add-to-list 'auto-mode-alist '("\\.phpt?$" . hack-mode))
 (provide 'j-php)
--- j-web.el
+++ j-web.el
@@ -1,6 +1,5 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
@@ -18,7 +17,5 @@
       web-mode-markup-indent-offset 2
       web-mode-script-padding 2
       web-mode-style-padding 2)
-
-
 
 (provide 'j-web)
