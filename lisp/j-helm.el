1c302e1b43c04b7ad055f3ae3acf15b995cbec9d
John Allen <jallen@fb.com>
2017-09-21 18:55:11 -0700

    tidy up helm config

--- lisp/j-helm.el
+++ lisp/j-helm.el
@@ -1,13 +1,21 @@
 (require 'helm)
 (require 'helm-config)
+
+;; M-x describe-bindings is now helm-y
 (require 'helm-descbinds)
 (helm-descbinds-mode)
 
-(require 'helm-company)
-(eval-after-load 'company
-  '(progn
-     (define-key company-mode-map (kbd "C-:") 'helm-company)
-     (define-key company-active-map (kbd "C-:") 'helm-company)))
+;; Sweet version of M-x
+(global-set-key (kbd "M-x") 'helm-M-x)
+(setq helm-M-x-fuzzy-match t)
+
+;; Visual kill-ring
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+
+;; Way better find-files. C-x 4 f is already bound somehow...
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (require 'yasnippet)
 (require 'helm-c-yasnippet)
@@ -15,22 +23,14 @@
 (global-set-key (kbd "C-c y") 'helm-yas-complete)
 (yas-global-mode 1)
 
-;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
-;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
-;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-unset-key (kbd "C-x c"))
-
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
-(when (executable-find "curl")
-  (setq helm-google-suggest-use-curl-p t))
-
-(when (executable-find "ack-grep")
-  (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
-        helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
+;; Figure out how to set ag as default grepper?
+;; (when (executable-find "ack-grep")
+;;   (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
+;;         helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
 
 (setq ;helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
       helm-autoresize-mode                  t
@@ -39,24 +39,5 @@
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
 
-(require 'helm-projectile)
-;(global-set-key (kbd "M-x") 'helm-M-x)
-
-(helm-projectile-on)
-
-
-(global-set-key (kbd "M-x") 'helm-M-x)
-(setq helm-M-x-fuzzy-match t)
-
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(setq helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t)
-
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-c C-f") 'helm-projectile)  ;; unshadow projectile-find-file
 (helm-mode 1)
-
-;; (global-set-key (kbd "C-s") 'helm-swoop)
-;; (global-set-key (kbd "C-S-s") 'helm-multi-swoop-all)
-
 (provide 'j-helm)
