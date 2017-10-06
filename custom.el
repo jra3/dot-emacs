803f7cf593c404fe553f3b4eedaef5066bc982ab
John Allen <jallen@fb.com>
2017-10-06 15:39:21 -0400

    literate org conversion complete

--- custom.el
+++ custom.el
@@ -5,4 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (load-dir yaml-mode win-switch whitespace-cleanup-mode web-mode wanderlust use-package thrift tangotango-theme rainbow-mode rainbow-delimiters popwin org-pomodoro minimap markdown-mode magit json-mode js2-refactor js-comint ibuffer-vc ibuffer-git htmlize helm-unicode helm-pydoc helm-projectile helm-orgcard helm-flycheck helm-flx helm-describe-modes helm-descbinds helm-css-scss helm-company helm-c-yasnippet google-c-style gitignore-mode gitconfig-mode exec-path-from-shell elpy elisp-slime-nav dired-details delight company-jedi color-identifiers-mode auto-complete anzu ag))))
+    (flycheck multiple-cursors helm-descbinds-mode load-dir yaml-mode win-switch whitespace-cleanup-mode web-mode wanderlust use-package thrift tangotango-theme rainbow-mode rainbow-delimiters popwin org-pomodoro minimap markdown-mode magit json-mode js2-refactor js-comint ibuffer-vc ibuffer-git htmlize helm-unicode helm-pydoc helm-projectile helm-orgcard helm-flycheck helm-flx helm-describe-modes helm-descbinds helm-css-scss helm-company helm-c-yasnippet google-c-style gitignore-mode gitconfig-mode exec-path-from-shell elpy elisp-slime-nav dired-details delight company-jedi color-identifiers-mode auto-complete anzu ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
