c0c77ebe464ea3a5a0992ce387e4bb2ceaac9e3a
John Allen <jallen@fb.com>
2018-02-08 14:02:12 -0800

    update

--- custom.el
+++ custom.el
@@ -29,9 +29,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
 
- '(newsticker-date-format "(%A %D %H:%M)")
- '(newsticker-html-renderer (quote shr-render-region))
- '(newsticker-use-full-width nil)
+ '(newsticker-date-format "(%A %D %H:%M)" t)
+ '(newsticker-html-renderer (quote shr-render-region) t)
+ '(newsticker-use-full-width nil t)
+
  '(package-selected-packages
    (quote
     (which-key hgrc-mode hgignore-mode w3 w3m flycheck multiple-cursors helm-descbinds-mode load-dir yaml-mode win-switch whitespace-cleanup-mode web-mode wanderlust use-package thrift tangotango-theme rainbow-mode rainbow-delimiters popwin org-pomodoro minimap markdown-mode magit json-mode js2-refactor js-comint ibuffer-vc ibuffer-git htmlize helm-unicode helm-pydoc helm-projectile helm-orgcard helm-flycheck helm-flx helm-describe-modes helm-descbinds helm-css-scss helm-company helm-c-yasnippet google-c-style gitignore-mode gitconfig-mode exec-path-from-shell elpy elisp-slime-nav dired-details delight company-jedi color-identifiers-mode auto-complete anzu ag)))
