f59ab9feceeeadd36e27bef8a76f509ec266f6ba
John Allen <jallen@fb.com>
2018-11-05 11:02:53 -0800

    cp

--- custom.el
+++ custom.el
@@ -28,14 +28,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
+ '(company-backends
+   (quote
+    (company-tasks company-reviewers company-bbdb company-nxml company-css company-capf
+                   (company-dabbrev-code company-keywords))))
+ '(company-minimum-prefix-length 1)
+ '(lsp-highlight-symbol-at-point t)
  '(newsticker-date-format "(%A %D %H:%M)" t)
  '(newsticker-html-renderer (quote shr-render-region) t)
  '(newsticker-use-full-width nil t)
-
  '(package-selected-packages
    (quote
-    (which-key hgrc-mode hgignore-mode w3 w3m flycheck multiple-cursors helm-descbinds-mode load-dir yaml-mode win-switch whitespace-cleanup-mode web-mode wanderlust use-package thrift tangotango-theme rainbow-mode rainbow-delimiters popwin org-pomodoro minimap markdown-mode magit json-mode js2-refactor js-comint ibuffer-vc ibuffer-git htmlize helm-unicode helm-pydoc helm-projectile helm-orgcard helm-flycheck helm-flx helm-describe-modes helm-descbinds helm-css-scss helm-company helm-c-yasnippet google-c-style gitignore-mode gitconfig-mode exec-path-from-shell elpy elisp-slime-nav dired-details delight company-jedi color-identifiers-mode auto-complete anzu ag)))
+    (cquery git-gutter hack-mode elpa-mirror lsp-hack package-lint company-lsp lsp-ui lsp-mode which-key hgrc-mode hgignore-mode w3 w3m flycheck multiple-cursors helm-descbinds-mode load-dir yaml-mode win-switch whitespace-cleanup-mode web-mode wanderlust use-package thrift tangotango-theme rainbow-mode rainbow-delimiters popwin org-pomodoro minimap markdown-mode magit json-mode js2-refactor js-comint ibuffer-vc ibuffer-git htmlize helm-unicode helm-pydoc helm-projectile helm-orgcard helm-flycheck helm-flx helm-describe-modes helm-descbinds helm-css-scss helm-company helm-c-yasnippet google-c-style gitignore-mode gitconfig-mode exec-path-from-shell elpy elisp-slime-nav dired-details delight company-jedi color-identifiers-mode auto-complete anzu ag)))
  '(shr-width 80))
 
 (custom-set-faces
