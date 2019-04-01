03f0bb54f24c946859ed839ccdb69bc347867719
John Allen <jallen@fb.com>
2019-03-31 22:22:02 -0400

    sync

--- custom.el
+++ custom.el
@@ -28,6 +28,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-backends
+   (quote
+    (company-tasks company-reviewers company-bbdb company-nxml company-css company-capf
+                   (company-dabbrev-code company-keywords))))
+ '(company-minimum-prefix-length 1)
  '(newsticker-date-format "(%A %D %H:%M)" t)
  '(newsticker-html-renderer (quote shr-render-region) t)
  '(newsticker-use-full-width nil t)
--- init.el
+++ init.el
@@ -12,31 +12,12 @@
 
 (defvar config-load-path (file-name-directory (or load-file-name buffer-file-name)))
 
-(require 'package)
-(setq package-archives nil)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-(eval-when-compile
-  (require 'use-package)
-					; install packages from package archive automatically
-  (require 'use-package-ensure)
-  (setq use-package-always-ensure t))
-
-(use-package auto-package-update
-	     :config
-	     (setq auto-package-update-delete-old-versions t)
-	     (setq auto-package-update-hide-results t)
-	     (auto-package-update-at-time "03:00"))
 
 
 (defvar config-org-files
-  '("local-before.org" ;; local preamble, .gitignored
+  '("local-bootstrap.org" ;; local preamble, .gitignored
+    "package-bootstrap.org"
+    "local-before.org" ;; local init, .gitignored
     "config.org"
     "local-after.org" ;; local coda, .gitignored
     ))
@@ -52,5 +33,3 @@
  config-org-files)
 
 (message "Start up time %.2fs" (float-time (time-subtract (current-time) my-start-time)))
-
-;;; init.el ends here
