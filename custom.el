06c895a9e79b579f791e0855d0dc1a1b838c101c
John Allen <jallen@fb.com>
2019-06-03 22:57:51 -0400

    update from server

--- custom.el
+++ custom.el
@@ -28,6 +28,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-package-update-delete-old-versions nil)
+ '(bbdb-file-remote "/usr/share/emacs/bbdb")
  '(company-backends
    (quote
     (company-tasks company-reviewers company-bbdb company-nxml company-css company-capf
