2b7a10f31a7cd22f2162c4db57fd162d7cd37dad
John Allen <jallen@fb.com>
2020-09-02 22:51:43 -0400

    more calsync

--- calsync.el
+++ calsync.el
@@ -1,10 +1,10 @@
 ;; $EMACS --batch -l ~/.emacs.d/calsync.el
 (let ((ics-files (directory-files-recursively "~/Library/Calendars" "\.ics$"))
       (diary-target "/tmp/diary-sync")
-      (diary-final "~/diary.cal"))
+      (diary-final "~/diary_cal"))
   (delete-file diary-target)
   (mapcar
    (lambda (ics-file)
      (icalendar-import-file ics-file diary-target))
    ics-files)
-  (rename-file diary-target diary-final t))
+  (shell-command (format "grep -v '^ ' %s | sort | uniq > %s" diary-final diary-target)))
